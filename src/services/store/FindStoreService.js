a915b3a9b9acee824f8ea3e5bb41ea7e92978551
eliasjay <zeliasgl@gmail.com>
2020-06-15 15:35:38 -0300

    add result query return

--- src/services/store/FindStoreService.js
+++ src/services/store/FindStoreService.js
@@ -3,18 +3,24 @@ const cassandra = require('../../infra/cassandra');
 class FindStoreService {
   async find() {
     const query = `SELECT * FROM market.store`;
+    let resQuery = '';
 
     await cassandra.execute(query, (result) => {
-      return result;
+      resQuery = result;
     });
+
+    return resQuery.rows;
   }
 
   async index(id) {
     const query = `SELECT * FROM market.store WHERE id=${id} ALLOW FILTERING`;
+    let resQuery = '';
 
     await cassandra.execute(query, (result) => {
-      return result;
-    })
+      resQuery = result;
+    });
+
+    return resQuery.rows;
   }
 }
 
